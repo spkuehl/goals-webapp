2bbec87d457a26cbaa13e44b7cea0d92f4fd4c2e
spkuehl <skuehlhorn@gmail.com>
2020-06-30 12:04:00 -0500

    Added comments for intended unit tests for goals app.

--- goals/tests.py
+++ goals/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# GOAL unit tests
+# Create Goal
+# Edit Goal
+# Deactivate Goal
+# Archive Goal
+# Get total duration (hours worked)
+
+# GOAL LOG unit tests
+# Create GoalLog
+# Edit GoalLog
+# Archive GoalLog
+# Add GoalLog.start_time
+# Edit GoalLog.start_time
+# Add GoalLog.end_time
+# Edit GoalLog.end_time
+# Get GoalLog duration == None if no start & end time.
+# Get GoalLog duration if start & end time.
