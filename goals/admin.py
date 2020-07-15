166de4049373cf728a0bb22154d510d9ca797377
spkuehl <skuehlhorn@gmail.com>
2020-07-15 15:35:00 -0500

    Added GoalLog to django admin.

--- goals/admin.py
+++ goals/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from goals.models import Goal
+from goals.models import Goal, GoalLog
+
 
 class GoalAdmin(admin.ModelAdmin):
     pass
 
+
+class GoalLogAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Goal, GoalAdmin)
+admin.site.register(GoalLog, GoalLogAdmin)
